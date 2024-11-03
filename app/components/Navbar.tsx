ddb014a7f8560bd5eea2803c7b3d4ec8d422f767
Ray Lam <raylamedeas@gmail.com>
2024-11-03 19:39:41 +0000

    Create Github Login & Logout function by Oauth

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,57 @@
 import React from 'react'
+import Link from 'next/link';
+import {auth, signIn, signOut} from "@/auth"; 
+import { redirect } from 'next/dist/server/api-utils';
 
-const Navbar = () => {
-  return (
-    <div>
-        Navbar
-    </div>
-  )
+const Navbar = async () => {
+    const session = await auth();
+
+    return (
+        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className='flex justify-between items-center'>
+                <Link href="/">
+                    Home
+                </Link>
+                <div className='flex items-center gap-5 text-black'>
+                    {session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span>
+                                    Create
+                                </span>
+                            </Link>
+
+                            <form action={async()=>{
+                                 "use server";
+
+                                 await signOut({redirectTo: "/"})
+                            }}>
+                                <button>
+                                    Logout
+                                </button>
+                            </form>
+
+                            <Link href={`/user/${session?.id}`}>
+                                <span>
+                                    {session?.user?.name}
+                                </span>
+                            </Link>
+                        </>
+                    ) : (
+                        <form action={ async() => {
+                            "use server";
+
+                            await signIn('github')
+                        }}>
+                            <button type="submit">
+                                Login
+                            </button>
+                        </form>
+                    )}
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar
