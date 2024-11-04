d52e437a8ecc55b5b726b1f00b7122ea98cd62a3
Ray Lam <raylamedeas@gmail.com>
2024-11-04 11:01:40 +0000

    Add: tailwindcss-animate, @tailwindcss/typography & update tailwind.config.ts

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = async () => {
 
                                  await signOut({redirectTo: "/"})
                             }}>
-                                <button>
+                                <button type="submit">
                                     Logout
                                 </button>
                             </form>
