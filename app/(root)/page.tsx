b4f691314b6581d383e191f5647d254cacc7d20e
Ray Lam <raylamedeas@gmail.com>
2024-11-05 14:02:32 +0000

    Add SearchForm & update Hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import SearchForm from "@/app/components/SearchForm";
 
 export default function Home() {
   return (
     <>
-      <h1 className="heading">HOME</h1>
+      <section className="pink_container">
+        <h1 className="heading">Testing with Next.js 15</h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ex aut eaque sunt nesciunt atque aliquam natus sit qui. Asperiores quia molestias dolores modi deserunt iste laudantium adipisci cum exercitationem.
+        </p>
+        <SearchForm />
+      </section>
     </>
   );
 }
