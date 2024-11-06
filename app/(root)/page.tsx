9f702edca5c346fd3c906149c334aa69122e1756
Ray Lam <raylamedeas@gmail.com>
2024-11-06 21:56:19 +0000

    Add PostCard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,20 @@
 import SearchForm from "@/components/SearchForm";
+import PostCard   from "@/components/PostCard";
 
 export default async function Home({searchParams}:{
   searchParams: Promise<{query?: string}>
 }) {
   const query = (await searchParams).query;
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: {_id: 1, name: 'John Doe'},
+    _id: 1,
+    description: 'This is a description.',
+    image: 'https://placehold.co/350x600',
+    category: "Robots",
+    title: "We Robots",
+  }];
   return (
     <>
       <section className="pink_container">
@@ -13,6 +24,22 @@ export default async function Home({searchParams}:{
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ex aut eaque sunt nesciunt atque aliquam natus sit qui. Asperiores quia molestias dolores modi deserunt iste laudantium adipisci cum exercitationem.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Items'}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: PostCardType, index: number)=>(
+              <PostCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">
+              No Post found
+            </p>
+          )}
+        </ul>
       </section>
     </>
   );
