d4703449cd94842bc2d68e77363bbfc000ecfbbc
Ray Lam <raylamedeas@gmail.com>
2024-11-07 18:37:05 +0000

    Update PostCard

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,9 +1,11 @@
 import { formatDate } from "@/lib/utils"
 import { EyeIcon } from "lucide-react"
 import Link from "next/link"
+import Image from "next/image"
+import { Button } from "./ui/button"
 
 const PostCard = ({post}:{post:PostTypeCard}) => {
-  const {_createdAt, views, author: {_id: authorId, name}, title, category, _id, image} = post;
+  const {_createdAt, views, author: {_id: authorId, name}, title, category, _id, image, description} = post;
   return (
     <li className="startup-card group">
       <div className="flex-between">
@@ -21,7 +23,35 @@ const PostCard = ({post}:{post:PostTypeCard}) => {
                         {name}
                     </p>
                 </Link>
+                <Link href={`/startup/${_id}`}>
+                  <h3 className="text-26-semibold line-clamp-1">
+                    {title} 
+                  </h3>
+                </Link>
             </div>
+            <Link href={`/usr/${authorId}`}>
+              <Image src="https://placehold.co/48x48" alt="placeholder"
+              width={48} height={48} className="rounded-full"></Image>
+            </Link>
+        </div>
+        <Link href={`/startup/${_id}`}>
+          <p className="startup-card_desc">
+            {description}
+          </p>
+
+          <img src={image} alt="placeholder" className="start-card_img" />
+        </Link>
+        <div className="flex-between gap-3 mt-5">
+          <Link href={`/?query=${category.toLowerCase()}`}>
+            <p className="text-16-medium">
+              {category}
+            </p>
+          </Link>
+          <Button className="start-card_btn" asChild>
+            <Link href={`/startup/${_id}`}>
+              Details
+            </Link>
+          </Button>
         </div>
       </div>
     </li>
