61f4f3a7d5090d8d4050500baf13d8f9d3e46f5f
Ray Lam <raylamedeas@gmail.com>
2024-11-10 22:35:39 +0000

    Add schemaTypes

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -4,4 +4,6 @@ import type {StructureResolver} from 'sanity/structure'
 export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
-    .items(S.documentTypeListItems())
+    .items([
+      S.documentTypeListItems('author').title('Authors')
+    ]);
